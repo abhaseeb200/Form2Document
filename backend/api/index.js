a8c036b01715747543b82d93870ae2221bb442bd
Abdul Haseeb <haseebabdul747@gmail.com>
2025-08-01 21:33:22 +0500

    Deployment: Rearrange folder structure

---
+++ backend/api/index.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { PDFDocument } = require("pdf-lib");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Server!");
+});
+
+app.post("/generate-document", async (req, res) => {
+  const data = req.body;
+  
+  try {
+    await fillPdf(data);
+    res
+      .status(200)
+      .json({
+        message: "Document generated successfully",
+        filePath: "/download",
+      });
+  } catch (error) {
+      console.error(error);
+    res.status(501).json({ message: "Not implemented yet" });
+  }
+});
+
+app.get("/download", (req, res) => {
+  const filePath = path.join(__dirname, "generated", "compliance-edit-filled.pdf");
+
+  if (!fs.existsSync(filePath)) {
+    console.error("File not found:", filePath);
+    return res.status(404).send("File not found");
+  }
+
+  res.download(filePath, (err) => {
+    if (err) {
+      console.error("Error downloading file:", err);
+      return res.status(500).send("Error downloading file");
+    }
+
+    console.log("File downloaded successfully");
+  });
+});
+    
+
+const fillPdf = async (data) => {
+  const bytes = fs.readFileSync("compliance-edit.pdf");
+  const pdfDoc = await PDFDocument.load(bytes);
+  const form = pdfDoc.getForm();
+
+  const addressField = form.getTextField("text_1upvx");
+  const priceField = form.getTextField("text_2aott");
+  const nameField = form.getTextField("text_3hfd");
+  const dateField = form.getTextField("text_5ttjw");
+  const addressField2 = form.getTextField("text_6brye");
+  const nameField2 = form.getTextField("text_7uprd");
+  const dateField2 = form.getTextField("text_8khjn");
+  const nameField3 = form.getTextField("text_9lzsn");
+  const dateField3 = form.getTextField("text_10krxw");
+
+  addressField.setText(data?.address || "");
+  priceField.setText(data?.price || "");
+  nameField.setText(data?.fullName || "");
+  dateField.setText(data?.date || "");
+  addressField2.setText(data?.address || "");
+  nameField2.setText(data?.fullName || "");
+  dateField2.setText(data?.date || "");
+  nameField3.setText(data?.fullName || "");
+  dateField3.setText(data?.date || "");
+
+  form.flatten();
+  const pdfBytes = await pdfDoc.save();
+
+  return fs.writeFileSync("generated/compliance-edit-filled.pdf", pdfBytes);
+};
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
